4e5c8a83e4f0459816afc1958fe1318776b17436
kdorian22 <kenneth.dorian@wustl.edu>
2018-01-20 12:41:36 -0600

    Add files via upload

---
+++ PitchSequence.R
@@ -0,0 +1,52 @@
+library(readxl)
+library(dplyr)
+library(Hmisc)
+Greenville <- read_excel("~/Desktop/Greenville.xlsx")
+Wesleyen <- read_excel("~/Desktop/Illinois Wesleyan.xlsx")
+iwID <- read_excel("~/Desktop/Illinois Wesleyan.xlsx", sheet=2)
+gID <- read_excel("~/Desktop/Greenville.xlsx", sheet=2)
+Case <- read_excel("~/Desktop/CWRU.xlsx")
+Cid <- read_excel("~/Desktop/CWRU.xlsx", sheet=2)
+
+
+merge2 <- merge(iwID, Wesleyen)
+total <- rbind(merge, merge2)
+##mintz <- total[which(total$pitcher=='mintz'),]
+##batter <- merge[which(merge$batter==unique(merge$batter)[1]),]
+info <- Case
+IDs <- Cid
+batter <- 'capetillo'
+pitcher <- 'margolin'
+atBat <- 1
+date <- "2017-04-17"
+pitchSequence <- function(info, IDs, batter, pitcher, atBat, date){
+  merge <- merge(IDs, info)
+  merge$date <- as.character(merge$date)
+  ID <- unique(merge$atBatID[which(merge$batter==batter & merge$pitcher==pitcher & merge$`at bat number`== atBat & merge$date==date)])
+  a <- subset(merge, merge$atBatID==ID)
+  colors <- c('red', 'blue', 'purple', 'green','orange','orange','orange')
+  cols <- c()
+  for(i in 1:max(a$`pitch of at bat`)){
+     cols[i] <- colors[a$`pitch type`[which(a$`pitch of at bat`==i)]]
+  }
+  x <- 1
+  for(i in 1:max(a$`pitch of at bat`)){
+      if(a$`strike/ball`[which(a$`pitch of at bat`==i)]==1){
+        plot(x,1,col=cols[i],pch=15,cex=4,axes=FALSE,xlab='',ylab='', xlim=range(0,10),ylim=range(0,2))
+      }
+      else{
+        plot(x,0.7,col=cols[i],pch=15,cex=4,axes=FALSE,xlab='',ylab='', xlim=range(0,10),ylim=range(0,2))
+      }
+    x <- x + 1
+    par(new=T)
+  }
+  text(x+1, 0.85, a$result[which(a$`pitch of at bat`==max(a$`pitch of at bat`))], cex=2)
+  text(c(0,0),c(1,0.7),c('S','B'), cex=1.5, font=4)
+  text(0,1.3, pos=4,paste0("P ", capitalize(a$pitcher[which(a$atBatID==a$atBatID[1])]), ";  B ", 
+                           capitalize(a$batter[which(a$atBatID==a$atBatID[1])]), ";  AB ", 
+                           (a$`at bat number`[which(a$atBatID[1]==a$atBatID[1])]), "; ", 
+                           a$date[which(a$atBatID==a$atBatID[1])]))
+
+  legend('bottomright', legend=c('FB', 'CB', 'SL', 'CH', '2S'), fill=c('red', 'blue', 'purple', 'green', 'orange'))
+}
+pitchSequence(Case, Cid, 'capetillo', 'howard', 2, "2017-04-14")
